619359facff695c987a3c96dacc10bf342e5abf5
Yannik Wassmuth <wassmuth@students.uni-marburg.de>
2023-01-24 11:19:06 +0100

    Renamend some functions

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -14,7 +14,7 @@ object Sindy {
       .option("delimiter", ";")
       .csv(input)
   }
-  def transform(row:Row): Array[(String, String)]={
+  def generateCells(row:Row): Array[(String, String)]={
     val names = row.schema.names
     val result = new Array[(String,String)](row.length)
     for(i <- 0 until row.length){
@@ -23,7 +23,7 @@ object Sindy {
     result
   }
 
-  def powerlist(list: List[String]):Array[(String,List[String])]={
+  def generateInclusionList(list: List[String]):Array[(String,List[String])]={
     val result = new Array[(String,List[String])](list.length)
     for(i <- 0 until list.length){
       var resultList:List[String] = List()
@@ -38,7 +38,7 @@ object Sindy {
     result
   }
 
-  def arrayToString(array: List[String]):String={
+  def listToString(array: List[String]):String={
     var result = ""
     for(s <- array) result += s + ", "
     result
@@ -48,40 +48,40 @@ object Sindy {
     import spark.implicits._
     println(inputs.length)
     var flattenInputs = readData(inputs(0), spark)
-      .flatMap(f => transform(f));
+      .flatMap(f => generateCells(f));
 
     var counter = 1;
     while(counter < inputs.length) {
       val flattenInput = readData(inputs(counter), spark)
-        .flatMap(f => transform(f))
+        .flatMap(f => generateCells(f))
       flattenInputs = flattenInputs.union(flattenInput);
       counter += 1;
     };
 
-      val result = flattenInputs
-        .groupByKey(a => a._1)
-        .mapGroups((s,i) => i.toList)
-        .map(l => l.map(t=> (t._2)).distinct)
-        .flatMap(l => powerlist(l))
-        .groupByKey(t => t._1)
-        .reduceGroups( (u,v) => (u._1, u._2.intersect(v._2:List[String])))
-        .map(u => u._2)
-        .filter(u => u._2.length > 0)
-        .map(u => (u._1, arrayToString(u._2)))
-        .toDF("Column", "Included In")
-        .sort("Column")
-        .map(u => (u(0) + " < " + u(1).toString))
-        .collect()
+    val result = flattenInputs
+      .groupByKey(a => a._1)
+      .mapGroups((s,i) => i.toList)
+      .map(l => l.map(t=> (t._2)).distinct)
+      .flatMap(l => generateInclusionList(l))
+      .groupByKey(t => t._1)
+      .reduceGroups( (u,v) => (u._1, u._2.intersect(v._2:List[String])))
+      .map(u => u._2)
+      .filter(u => u._2.length > 0)
+      .map(u => (u._1, listToString(u._2)))
+      .toDF("Column", "Included In")
+      .sort("Column")
+      .map(u => (u(0) + " < " + u(1).toString))
+      .collect()
 
     //OUTPUT-----------
-      println("RESULTS:")
+    println("RESULTS:")
 
-      counter = 0
-      for(l <- result) {
-        counter = counter + l.count(_ == ',')
-        println(l)
-      }
+    counter = 0
+    for(l <- result) {
+      counter = counter + l.count(_ == ',')
+      println(l)
+    }
 
-      println("Total number of INGs: " + counter)
+    println("Total number of INDs: " + counter)
   }
 }
